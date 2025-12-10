3be140812593f844be7983b603465a74b731fea4
choiwjun <wj94128871@gmail.com>
2025-12-10 14:45:07 +0900

    fix: Add ScrollToTop component to scroll to page top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,12 @@ import { VisitPage } from './components/VisitPage';
 import { LocationPage } from './components/LocationPage';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { ScrollToTop } from './components/ScrollToTop';
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">
